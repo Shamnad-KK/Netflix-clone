dfbbcc28038ab1449086f26c7bf8c1b13584b3f1
Shamnad KK <shamnadchemmu702@gmail.com>
2022-08-24 20:08:20 +0530

    created some  model class

---
+++ lib/core/strings.dart
@@ -0,0 +1,1 @@
+const baseURL = "https://api.themoviedb.org/3";
---
+++ lib/domain/core/api_end_points.dart
@@ -0,0 +1,6 @@
+import 'package:netflix_clone/core/strings.dart';
+import 'package:netflix_clone/infrastructure/api_key.dart';
+
+class ApiEndPoints {
+  static const downloads = "$baseURL/trending/all/day?api_key=$apiKey";
+}
